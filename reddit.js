01791cd0a040ce8b82fe0df59c882858abc58c26
Chris Stiles <chrisstiles92@gmail.com>
2020-03-10 11:32:30 -0700

    Fix comment

--- reddit.js
+++ reddit.js
@@ -260,7 +260,7 @@ function submitComment(submission, publishDate, modifyDate, data, url) {
       The original publication date was ${publishDate.format('MMMM Do, YYYY')}${modifyText}. ${text}
       &nbsp;  
       &nbsp;  
-      ^(This bot finds outdated articles. It only checks certain subreddits, but) this [^(Chrome extension)](https://chrome.google.com/webstore/detail/reddit-publish-date/cfkbacelanhcgpkjaocblkpacofnccip?hl=en-US) ^(will check links on all subreddits. It's impossible to be 100% accurate on every site, and with differences in time zones and date formats this may be a little off. Send me a message if you notice an error or would like this bot added to your subreddit.)
+      ^(This bot finds outdated articles. It only checks certain subreddits, but this [^(Chrome extension)](https://chrome.google.com/webstore/detail/reddit-publish-date/cfkbacelanhcgpkjaocblkpacofnccip?hl=en-US) ^(will check links on all subreddits. It's impossible to be 100% accurate on every site, and with differences in time zones and date formats this may be a little off. Send me a message if you notice an error or would like this bot added to your subreddit.)
 
       [^(Send Feedback)](https://www.reddit.com/message/compose?to=PublishDateBot)  ^(|)  [^(Github - Bot)](https://github.com/chrisstiles/PublishDateBot)  ^(|)  [^(Github - Chrome Extension)](https://github.com/chrisstiles/Reddit-Publish-Date)
     `;
