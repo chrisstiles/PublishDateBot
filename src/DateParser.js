0807e9f908e8a8e680dedf1c08c767760c54a7f8
Chris Stiles <chrisstiles92@gmail.com>
2022-10-16 16:28:44 -0700

    Remove preinstall script

--- src/DateParser.js
+++ src/DateParser.js
@@ -44,7 +44,7 @@ export default class DateParser {
     if (this.enablePuppeteer && !this.browser) {
       this.browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
       });
     }
 
